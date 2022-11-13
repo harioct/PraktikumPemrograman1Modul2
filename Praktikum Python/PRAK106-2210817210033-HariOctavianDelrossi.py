dac44a59fc4b95d1726e8c67617fcf61edb508b4
harioct <114865767+harioct@users.noreply.github.com>
2022-11-13 12:31:56 +0700

    Update PRAK106-2210817210033-HariOctavianDelrossi.py

--- Praktikum Python/PRAK106-2210817210033-HariOctavianDelrossi.py
+++ Praktikum Python/PRAK106-2210817210033-HariOctavianDelrossi.py
@@ -1,16 +1,18 @@
 a=4
 b=8
 c=3
-print('Variabel a bernilai ', a)
-print('Variabel b bernilai ', b)
-print('Variabel c bernilai ', c)
-bool_t = True
-int_1 = int(bool_t)
-bool_f = False
-int_0 = int(bool_f)
-x = a==b 
-y = b>=c
-z = a!=c
-print('Apakah a sama dengan b? Jawabannya adalah ', round(x))
-print('Apakah b lebih besar dari c? Jawabannya adalah ', round(y))
-print('Apakah a tidak sama dengan c? Jawabannya adalah ', round(z))
+print("Variabel a bernilai ", a)
+print("Variabel b bernilai ", b)
+print("Variabel c bernilai ", c)
+if a==b:
+    print("Apakah a sama dengan b? Jawabannya adalah 1")
+else:
+    print("Apakah a sama dengan b? Jawabannya adalah 0")
+if b>c:
+    print("Apakah b lebih besar dari c? Jawabannya adalah 1")
+else:
+    print("Apakah b lebih besar dari c? Jawabannya adalah 0")
+if a!=c:
+    print("Apakah a tidak sama dengan c? Jawabannya adalah 1")
+else:
+    print("Apakah a tidak sama dengan c? Jawabannya adalah 0")
